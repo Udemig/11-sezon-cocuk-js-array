b876d11e7f4cb7d2af8d330d2ac427e50b261c9b
omeryasindemir <omeryasindemir66@gmail.com>
2024-02-05 13:42:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,105 @@
+// baglantı kontrolu
+// console.log("baglandi")
+
+// -------------------------------------
+
+// kötü liste kullanımı
+
+// let araba1 = "araba-marka-1"
+// let araba2 = "araba-marka-2"
+// let araba3 = "araba-marka-3"
+
+// console.log(araba1)
+
+// -------------------------------------
+
+// basit obje ve liste oluşturma
+// const araba_listesi = ["araba-13", "araba-2", "araba-3"];
+
+// console.log(araba_listesi);
+
+// -------------------------------------
+
+// arabaların içine yazmadan içeriği değiştirme
+// const arabalar = [];
+
+// arabalar[0] = "araba-marka-1"
+// arabalar[1] = "araba-marka-2"
+// // arabalar[2] = "araba-marka-3"
+
+// console.log(arabalar)
+
+// -------------------------------------
+
+// yeni liste oluşturma
+
+// const arabalar = new Array("araba-marka-1", "araba-marka-2", "araba-marka-3");
+// console.log(arabalar)
+
+// -------------------------------------
+
+// listenin içindeki elemana ulaşmak
+
+// const arabalar = ["araba-1", "araba-2", "araba-3"]
+
+// console.log(arabalar[1])
+
+// -------------------------------------
+
+// listenin elemanını değiştirmek
+
+// const arabalar = ["araba-1", "araba-2", "araba-3"]
+
+// arabalar[0] = "opel"
+
+// console.log(arabalar)
+
+// -------------------------------------
+
+// BURAYA KADAR İŞLENENLER
+// 1. liste oluşturma
+// 2. liste elemanlarını ekleme
+// 3. liste elemanlarını değiştirme
+// bunların hepsini konsol üzerinden yaptık
+
+// -------------------------------------
+
+// listeyi ekrana yazdırma
+
+// const meyveler = ["Muz", "Elma", "Armut"]
+// document.getElementById("meyveler").innerHTML = meyveler.toString()
+
+// -------------------------------------
+
+// javascript ile div içeriğini değiştirdik
+
+// const meyveler = ["Muz", "Elma", "Armut"]
+// meyveler[0] = "Kivi"
+// document.getElementById("meyveler").innerHTML = meyveler.toString()
+
+// -------------------------------------
+
+// const users_2 = {
+//   isim: "Roket",
+//   soyisim: "Atar",
+//   yas: 46,
+// };
+
+// console.log(users_2.soyisim)
+
+// document.getElementById("users").innerHTML = `
+//     <div>İsim : ${users_2.isim}</div>
+//     <div>Soyisim : ${users_2.soyisim}</div>
+//     <div>Yaş : ${users_2.yas}</div>
+// `;
+
+// console.log(users_2);
+// console.log("İsim : ", users_2.isim);
+// console.log("Soyisim : ", users_2.isim);
+// console.log("Yaş : ", users_2.isim);
+
+// -------------------------------------
+
+// listenin kaç elemanlı olduğunu görme
+// const meyveler = ["armut", "kivi", "elma", "karpuz"]
+// console.log(meyveler.length)
